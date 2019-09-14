b9ca95b94f7f1134f7c2613f82ef7739b8f83339
Muhammad Irfan <virtuoso.irfan@gmail.com>
2019-09-14 11:09:11 +0500

    cleanup

--- synthesizer/views.py
+++ synthesizer/views.py
@@ -44,22 +44,3 @@ class TranscriptView(View):
                 pk = pk + ".wav"
             return JsonResponse({"success": True, "path": str("/media/wav/{}".format(pk))}, status=200)
         return JsonResponse({"success": False}, status=400)
-
-
-def get_transcript(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = TranscriptForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # redirect to a index URL:
-            data = form.cleaned_data['transcript']
-            return HttpResponseRedirect('/')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = TranscriptForm()
-
-    return render(request, 'transcript.html', {'form': form})
